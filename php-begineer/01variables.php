980817e54f91c9c557443a985b68833d2773f17a
Subhankar Ghosh <subhankarghosh911@gmail.com>
2025-08-11 13:03:16 +0530

    Learn varibale & functions

--- php-begineer/01variables.php
+++ php-begineer/01variables.php
@@ -3,6 +3,28 @@
 $name="jhon";
 $age=20;
 $money=100.99;
+$isSalary=null;
 
-echo $name  . "<br>";
-echo "hello {$name}" .$age;
+
+
+//print the variables
+// echo $name  . "<br>";
+// echo "hello {$name}" .$age . '<br>';
+
+// //get type of variables
+// echo gettype($name) . "<br>";
+// echo gettype($age) . "<br>";
+
+//get print the whole variable
+// print_r($name);
+// echo "<br>";
+
+// var_dump($name);
+// echo "<br>";
+
+//variable checking functions
+var_dump(is_string($name));
+echo "<br>";
+var_dump(is_int($age));
+echo "<br>";
+
