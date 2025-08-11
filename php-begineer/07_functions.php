3615301558c99a6256182242c8951bf818271ac2
Subhankar Ghosh <subhankarghosh911@gmail.com>
2025-08-11 15:15:03 +0530

    learn function with parameter

--- php-begineer/07_functions.php
+++ php-begineer/07_functions.php
@@ -1,1 +1,19 @@
 <?php
+
+// function sayHello(){
+//     echo "hello world";
+// }
+// sayHello();
+
+//with parameters
+
+// function greet($name){
+//     echo "hello  , $name!";
+// }
+// greet("Subhankar");
+
+function greet($name ="guest"){
+    echo "hello  , $name! <br>";
+}
+greet();
+greet("Subhankar");
